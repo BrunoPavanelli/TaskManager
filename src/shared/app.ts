39750edf4d5225946bc526219f6024906110d07c
BrunoPavanelli <brunopavanellicontato@gmail.com>
2023-08-12 13:39:10 -0300

    chore(shared): put all app/servers files in shared folder and update imports

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -2,9 +2,9 @@ import express, { json } from 'express';
 import "reflect-metadata"
 import "express-async-errors"
 
-import { usersRoute } from './users/routes/users.routes';
-import { tasksRoute } from './tasks/routes/tasks.routes';
-import { permissionsRoute } from './permissions/routes/permissions.routes';
+import { usersRoute } from '../users/routes/users.routes';
+import { tasksRoute } from '../tasks/routes/tasks.routes';
+import { permissionsRoute } from '../permissions/routes/permissions.routes';
 
 const app = express()
 app.use(json())
