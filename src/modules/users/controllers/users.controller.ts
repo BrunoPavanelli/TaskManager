a5196c2d2ab2fd9c19516fe668a61f55d17636d5
BrunoPavanelli <brunopavanellicontato@gmail.com>
2023-08-13 14:42:46 -0300

    refactor(users): remove users services file and connect repository directly in controller

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,45 +1,50 @@
 import { Request, Response } from "express";
-import { UsersService, usersServices } from "../services/users.service";
+import { UsersRepository } from "../repositories/users.repository";
+import { schemas } from "../schemas/users.schemas";
+import { usersRepository } from "../repositories/typeorm/typeorm.users.repository";
 
 class UsersContoller {
-    constructor(private usersService: UsersService) {}
+    constructor(private usersRepository: UsersRepository) {}
 
     async create(req: Request, res: Response): Promise<Response> {
         const userData = req.body
-        const user = await this.usersService.create(userData);
+        const user = await this.usersRepository.create(userData);
 
-        return res.status(201).json(user);
+        const userResponse = schemas.response.parse(user)
+        return res.status(201).json(userResponse);
     }
 
     async findAll(req: Request, res: Response): Promise<Response> {
-        const users = await this.usersService.findAll();
+        const users = await this.usersRepository.findAll();
 
-        return res.json(users);
+        const usersResponse = users.map(user => schemas.response.parse(user))
+        return res.json(usersResponse);
     }
 
     async findById(req: Request, res: Response): Promise<Response> {
         const { user } = req.body
-        const userResponse = await this.usersService.findById(user)
 
+        const userResponse = schemas.response.parse(user)
         return res.json(userResponse);
     }
 
     async updateById(req: Request, res: Response): Promise<Response> {
         const { user } = req.body
         const userData = req.body
-        const userResponse = await this.usersService.updateById(user, userData);
+        const userPatched = await this.usersRepository.updateById(user, userData);
 
+        const userResponse = schemas.response.parse(userPatched)
         return res.json(userResponse)
     }
 
     async deleteById(req: Request, res: Response): Promise<Response> {
         const { user } = req.body
-        await this.usersService.deleteById(user);
+        await this.usersRepository.deleteById(user);
 
         return res.sendStatus(204);
     }
 
 }
 
-const usersController = new UsersContoller(usersServices);
+const usersController = new UsersContoller(usersRepository);
 export { usersController };
