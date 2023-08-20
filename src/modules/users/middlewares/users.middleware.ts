3ac6f6c486ddb5b60bfe25e9174f89db76008d64
BrunoPavanelli <brunopavanellicontato@gmail.com>
2023-08-20 16:10:41 -0300

    feat/users: implement ensure correct credentials middleware

--- src/modules/users/middlewares/users.middleware.ts
+++ src/modules/users/middlewares/users.middleware.ts
@@ -3,6 +3,8 @@ import { NextFunction, Request, Response } from "express";
 
 import { AppDataSource } from "../../../shared/data-source";
 import { User } from "../entities/users.entity";
+import { TLogin } from "../interfaces/users.interfaces";
+import { compareSync } from "bcryptjs";
 
 class UsersMiddleware {
     private repository: Repository<User> = AppDataSource.getRepository(User);
@@ -16,6 +18,26 @@ class UsersMiddleware {
         if (!user) return res.status(404).json({"message": "User not Found!"})
 
         req.body.user = user;
+        return next();
+    }
+    
+    async ensureCorrectCredentials(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        const userData: TLogin = req.body;
+
+        const user = await this.repository.findOneBy({
+            email: userData.email
+        });
+
+        if (!user) return res.status(401).json({message: "Invalid credentials"});
+
+        const userPassword: string = user.password;
+        const userRequestPassword: string = userData.password;
+
+        const validatePassword: boolean = compareSync(userRequestPassword, userPassword);
+        if (!validatePassword) return res.status(401).json({message: "Invalid credentials"});
+
+        res.locals.user = user;
+
         return next();
     }
 }
